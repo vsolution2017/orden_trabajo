aa1988680e9b031d370bbee7046ac1d5ba5bc7b0
CarSeb <CarSeb@DESKTOP-J48448P>
2017-09-30 02:06:06 -0500

    30/09 2:05

--- resource/js/style.js
+++ resource/js/style.js
@@ -1,21 +1,28 @@
 $(function () {
-    /* Style Tab Actividades */
+    /* Style Tab Maquinas*/
+    $("#tab_maquinaria").on("change", ".input-area", function () {
+        SumarAreas(".input-area", ".contenedor-area", ".total-area-viaje",this);
+    });
+    $("#tab_maquinaria").on("change", ".total-area-viaje", function () {
+        SumarCosto(".content .row:not(.hidden) .total-area-viaje", ".actividad_sample", ".total_viajes", this, 1);
+    });
+    /* Style Tab Maquinas */
+    
+    /* Style Tab Actividades Relleno*/
     $("#tab_actividades").on("change", ".input-area", function () {
-        producto = 1;
-        $(this).closest('.contenedor-area').find(".input-area").each(function (i, value) {
-            num = parseFloat($(value).val());
-            if (num > 0) {
-                producto *= num;
-            }
-        });
-        $(this).closest('.contenedor-area').find(".total-area").val(producto.toFixed(2)).change();
+        SumarAreas(".input-area", ".contenedor-area", ".total-area",this);
     });
     $("#tab_actividades").on("change", ".total-area", function () {
-        sum = 0;
-        $(this).closest(".actividad_sample").find(".content .row:not(.hidden) .total-area").each(function (i, v_ta) {
-            sum += parseFloat($(v_ta).val());
-        });
-        $(this).closest(".actividad_sample").find(".v_total-area").val(sum.toFixed(2));
+        SumarCosto(".content .row:not(.hidden) .total-area", ".actividad_sample", ".total-relleno", this, 0);
+    });
+    /* Style Tab Actividades */
+    
+    /* Style Tab Actividades Pavimento*/
+    $("#tab_actividades").on("change", ".input-area", function () {
+        SumarAreas(".input-area", ".contenedor-area", ".total-area-pav",this);
+    });
+    $("#tab_actividades").on("change", ".total-area-pav", function () {
+        SumarCosto(".content .row:not(.hidden) .total-area-pav", ".actividad_sample", ".total-pavimento", this, 2);
     });
     /* Style Tab Actividades */
 
@@ -49,59 +56,100 @@ $(function () {
     $(":input").inputmask();
 
     $("#btn_add_maq").click(function () {
-        ValidationMaquinaria("#cbo_maq :selected", "#maquinaria_select", "#op_add_maq", true);        
+        ValidationMaquinaria("#cbo_maq :selected", "#maquinaria_select", "#op_add_maq", true);
     });
     
+    
+
     $("#btn_add_mano_obra").click(function () {
-        ValidationMaquinaria("#cbo_mano_obra :selected", "#mano_obra_select", "#op_add_mano_obra", false);        
+        ValidationMaquinaria("#cbo_mano_obra :selected", "#mano_obra_select", "#op_add_mano_obra", false);
     });
-    
+
     $("#btn_save_hr").click(function () {
         hr_ini = $(".modal_horas").find("input.time_ini").toArray();
         hr_fin = $(".modal_horas").find("input.time_fin").toArray();
-        c = hr_ini.length -1;
+        c = hr_ini.length - 1;
         totalhora = 0;
         totalmin = 0;
         for (var i = 0; i < c; i++) {
             hora_inicio = hr_ini[i].value;
             hora_final = hr_fin[i].value;
-            var startTime=moment(hora_inicio, "HH:mm");
-            var endTime=moment(hora_final, "HH:mm");            
+            var startTime = moment(hora_inicio, "HH:mm");
+            var endTime = moment(hora_final, "HH:mm");
             var totalHours = (endTime.diff(startTime, 'hours'));
             var totalMinutes = endTime.diff(startTime, 'minutes');
             var clearMinutes = totalMinutes % 60;
             totalhora += totalHours;
             totalmin += clearMinutes;
         }
-        if(totalmin >= 60){
-            totalhora = totalhora + (totalmin/60);
+        if (totalmin >= 60) {
+            totalhora = totalhora + (totalmin / 60);
             totalmin = totalmin % 60;
         }
+
         costog = $(".maq_costo_gall").find("input.costo_gallineta").val();
-        costob = $(".maq_costo_bobc").find("input.costo_bobcat").val();
         vtotal_g = CalcularTiempo(totalhora, totalmin, costog);
+
+        costob = $(".maq_costo_bobc").find("input.costo_bobcat").val();
         vtotal_b = CalcularTiempo(totalhora, totalmin, costob);
-        
-        if(totalhora.toString().length < 2){
-            totalhora = "0"+totalhora;
+
+        if (totalhora.toString().length < 2) {
+            totalhora = "0" + totalhora;
         }
-        if(totalmin.toString().length < 2){
-            totalmin = "0"+totalmin;
+        if (totalmin.toString().length < 2) {
+            totalmin = "0" + totalmin;
         }
-        time = totalhora+":"+totalmin;
+        time = totalhora + ":" + totalmin;
         $(".maq_costo_gall").find("input.maq_time_gall").val(time);
         $(".maq_costo_gall").find("input.total_gallineta").val(vtotal_g);
-        
+
         $(".maq_costo_bobc").find("input.maq_time_bobc").val(time);
         $(".maq_costo_bobc").find("input.total_bobcat").val(vtotal_b);
-        
+
     });
 
     //tab_maquinaria
     $("#tab_maquinaria").on("click", ".delete", function () {
         $(this).closest(".input-group").remove();
         index = $(this).closest(".input-group").find("button.disabled");
-        OcultarPanelActividadCosto("#maquinaria_select",".maq_costo", ".maq_panel", index);
+        OcultarPanelActividadCosto("#maquinaria_select", ".maq_costo", ".maq_panel", index);
+    });
+    
+    $("label.btn-normal").click(function () {
+        arr = $(".btn-compacto").hasClass("active");
+        if(arr === false){
+            $(".btn-add-normal-compacto").addClass("hidden");
+            $(".btn-dlt-normal-compacto").addClass("hidden");
+        }
+    });
+    
+    $("label.btn-compacto").click(function () {
+        arr = $(".btn-compacto").hasClass("active");
+        if(arr === false){
+            $(".btn-add-normal-compacto").removeClass("hidden");
+            $(".btn-dlt-normal-compacto").removeClass("hidden");
+        }else{
+            $(".btn-add-normal-compacto").addClass("hidden");
+            $(".btn-dlt-normal-compacto").addClass("hidden");
+        }
+    });
+    
+    $("label.btn-veinte").on('click', function() {
+        $(".input-group").find("input.costo_pavimento").val("19.83");
+        arr = $(".btn-veinte").hasClass("active");
+        if(arr === false){
+            $(".btn-add-veinte-quince").removeClass("hidden");
+            $(".btn-dlt-veinte-quince").removeClass("hidden");
+        }
+    });
+    
+    $("label.btn-quince").on('click', function() {
+        $(".input-group").find("input.costo_pavimento").val("14.87");
+        arr = $(".btn-quince").hasClass("active");
+        if(arr === false){
+            $(".btn-add-veinte-quince").removeClass("hidden");
+            $(".btn-dlt-veinte-quince").removeClass("hidden");
+        }
     });
 
     /* Carlos */
@@ -113,11 +161,11 @@ $(function () {
         $(this).closest(".actividad_sample").find(".content").append(op_area);
 
     });
-    
+
     $("#tab_actividades").on("click", ".delete", function () {
         $(this).closest(".row").remove();
     });
-    
+
     $("#tab_actividades").on("click", ".edit", function () {
         $(this).removeClass("btn-info");
         $(this).addClass("btn-success");
@@ -127,7 +175,7 @@ $(function () {
         $(this).removeClass("edit");
         $(this).addClass("save");
     });
-    
+
     $("#tab_actividades").on("click", ".save", function () {
         $(this).addClass("btn-info");
         $(this).removeClass("btn-success");
@@ -139,7 +187,7 @@ $(function () {
     });
 });
 
-function ValidationMaquinaria (param1, param2, param3, param4) {
+function ValidationMaquinaria(param1, param2, param3, param4) {
     maquina_nombre = $(param1).text();
     op2_clone = $(param2).find("button.disabled").toArray();
     for (var i = 0; i < op2_clone.length; i++) {
@@ -157,8 +205,8 @@ function ValidationMaquinaria (param1, param2, param3, param4) {
         $(op_clone).removeClass("hidden");
         $(param2).append(op_clone);
     }
-    if(param4){
-        MostrarPanelActividadCosto(param2,".maq_costo", ".maq_panel");
+    if (param4) {
+        MostrarPanelActividadCosto(param2, ".maq_costo", ".maq_panel");
     }
 }
 
@@ -167,20 +215,22 @@ function MostrarPanelActividadCosto(param1, param2, param3) {
     for (var i = 0; i < op2_clone.length; i++) {
         op3_clone = op2_clone[i].innerHTML;
         if ((op3_clone === "Retroexcavadora")) {
-            band = 1; 
-        } else  if(op3_clone === "Martillo Neumatico"){
+            band = 1;
+        } else if (op3_clone === "Martillo Neumatico") {
             band = 0;
-        }else{
+        } else {
             band = -1;
         }
     }
-    if(band === 1 || band === 0){
+    if (band === 1 || band === 0) {
         $(param2).removeClass("hidden");
         $(param3).removeClass("hidden");
-        if(band === 1){
-            $(".maq_costo_gall").removeClass("hidden");   
-        }else{
-            $(".maq_costo_bobc").removeClass("hidden");   
+        if (band === 1) {
+            $(".maq_costo_gall").removeClass("hidden");
+            $("label.btn-gallineta").addClass("active");
+        } else {
+            $(".maq_costo_bobc").removeClass("hidden");
+            $("label.btn-bobcat").addClass("active");
         }
     }
 }
@@ -193,47 +243,88 @@ function OcultarPanelActividadCosto(param1, param2, param3, param4) {
     for (var i = 0; i < cont; i++) {
         op3_clone = op2_clone[i].innerHTML;
         if ((op3_clone === "Retroexcavadora") || (op3_clone === "Martillo Neumatico")) {
-            doble += 1; 
+            doble += 1;
         }
         if ((index === "Retroexcavadora")) {
             band = 1;
-        } else  if(index === "Martillo Neumatico"){
+        } else if (index === "Martillo Neumatico") {
             band = 0;
-        }else{
+        } else {
             band = -1;
         }
-    }  
-    if(band === -1){
-        if(cont === 1){
+    }
+    if (band === -1) {
+        if (cont === 1) {
             $(param2).addClass("hidden");
             $(param3).addClass("hidden");
             $(".maq_costo_bobc").addClass("hidden");
             $(".maq_costo_gall").addClass("hidden");
-        }   
-    }else{
-        if((cont === 1  && doble <= 1) || (cont > 1  && doble === 0)){
+        }
+    } else {
+        if ((cont === 1 && doble <= 1) || (cont > 1 && doble === 0)) {
             $(param2).addClass("hidden");
             $(param3).addClass("hidden");
         }
-        if(band === 1){
-            $(".maq_costo_gall").addClass("hidden");   
+        if (band === 1) {
+            $(".maq_costo_gall").addClass("hidden");
+            $("label.btn-gallineta").removeClass("active");
         }
-        if(band === 0){
-            $(".maq_costo_bobc").addClass("hidden");   
+        if (band === 0) {
+            $(".maq_costo_bobc").addClass("hidden");
+            $("label.btn-bobcat").removeClass("active");
         }
     }
 }
 
 function CalcularTiempo(hora, minuto, costo) {
-    total =  0.00;
+    total = 0.00;
     vhora = 0;
     vmin = 0;
-    if(hora > 0){
+    if (hora > 0) {
         vhora = parseFloat(hora * costo);
     }
-    if(minuto > 0){
-        vmin = parseFloat((costo * minuto)/0.6)/100;
-    }    
+    if (minuto > 0) {
+        vmin = parseFloat((costo * minuto) / 0.6) / 100;
+    }
     total = vhora + vmin;
     return total.toFixed(2);
+}
+
+function SumarAreas(param1, param2, param3,param4) {
+    producto = 1;
+    $(param4).closest(param2).find(param1).each(function (i, value) {
+        num = parseFloat($(value).val());
+        if (num > 0) {
+            producto *= num;
+        }
+    });
+    $(param4).closest(param2).find(param3).val(producto.toFixed(2)).change();
+}
+
+function SumarCosto(param1, param2, param3, param4, param5) {    
+    sum = 0;
+    $(param4).closest(param2).find(param1).each(function (i, v_ta) {
+        sum += parseFloat($(v_ta).val());        
+    });
+    if(param5 === 0){
+       $(param4).closest(param2).find(".v_total-area").val(sum.toFixed(2));
+       total_area = sum.toFixed(2);
+       costo = $(param4).closest(param2).find(".costo_relleno").val();
+       sum = total_area * costo;
+    }
+    if(param5 === 1){
+       sum = (sum *1.2)/8;
+       $(param4).closest(param2).find(".num_viaje").val(sum.toFixed(2));
+       viajes = sum.toFixed(2);
+       costo = $(param4).closest(param2).find(".costo_viajes").val();
+       sum = viajes * costo;
+    }
+    if(param5 === 2){
+       $(param4).closest(param2).find(".v_total-area_pav").val(sum.toFixed(2));
+       total_area = sum.toFixed(2);
+       costo = $(param4).closest(param2).find(".costo_pavimento").val();
+       sum = total_area * costo;
+    }
+    
+    $(param4).closest(param2).find(param3).val(sum.toFixed(2));
 }
